255c835bf58b60760843064af6f9215ca80de588
JerryLiu <liurun637@gmail.com>
2026-05-26 11:34:19 +0800

    fix: remove --disable-software-rasterizer and --disable-gpu-compositing
    These flags caused white screen on Linux systems without GPU.
    Software rasterizer is the essential fallback renderer and should
    not be disabled. GPU compositing should also be left enabled as
    Chromium will fall back to software automatically when needed.
    
    Keep: --no-sandbox, --disable-setuid-sandbox, --disable-gpu-sandbox,
    --disable-dev-shm-usage (these are safe and necessary for Linux).

--- app/gui-js/main.js
+++ app/gui-js/main.js
@@ -7,8 +7,6 @@ const yaml = require('js-yaml')
 app.commandLine.appendSwitch('no-sandbox')
 app.commandLine.appendSwitch('disable-setuid-sandbox')
 app.commandLine.appendSwitch('disable-gpu-sandbox')
-app.commandLine.appendSwitch('disable-software-rasterizer')
-app.commandLine.appendSwitch('disable-gpu-compositing')
 app.commandLine.appendSwitch('disable-dev-shm-usage')
 
 const userDataPath = app.getPath('userData')
